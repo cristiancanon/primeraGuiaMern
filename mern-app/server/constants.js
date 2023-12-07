5feda52cab7bfbaf4e664b8dcd28bcc5da2f5282
cristiancanon <124898592+cristiancanon@users.noreply.github.com>
2023-12-07 12:18:12 -0500

    Guiamern1

---
+++ mern-app/server/constants.js
@@ -0,0 +1,18 @@
+const DB_USER = "cristian"
+const DB_PASSWORD = "cristian-11"
+const DB_HOST = "mern-web-personal.b2xjlwh.mongodb.net"
+
+const API_VERSION = "v1"
+const IP_SERVER = "localhost"
+
+
+const JWT_SECRET_KEY = 'marin123negro'
+
+module.exports = {
+    DB_USER, 
+    DB_PASSWORD, 
+    DB_HOST,
+    API_VERSION,
+    IP_SERVER,
+    JWT_SECRET_KEY
+}
